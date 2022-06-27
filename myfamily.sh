9a8a046178ea63a365aabc002b25edde428353ca
ALANN <SchnebelenAlann@hotmail.com>
2022-06-27 19:43:57 +0200

    My very first commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,2 @@
-curl https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select(.id == 1)  | .connections | .relatives  ' -r
+import HERO_ID
+curl https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select(.id == HERO_ID)  | .connections | .relatives  ' -r
