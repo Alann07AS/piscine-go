48a798bf176909ea1e6d480a9575fc62fdaf24e4
ALANN <SchnebelenAlann@hotmail.com>
2022-06-27 20:10:27 +0200

    My very first commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,1 @@
-export HERO_ID
-curl https://zone01normandie.org/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" ' .[] | select(.id == $HERO_ID)  | .connections | .relatives  ' -r
+curl https://zone01normandie.org/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" ' .[] | select(.id == contain('$HERO_ID'))  | .connections | .relatives  ' -r
