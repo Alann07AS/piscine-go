ec13fec53db48651b68a8be8cddde983e2d4b335
ALANN <SchnebelenAlann@hotmail.com>
2022-06-27 20:06:13 +0200

    My very first commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-curl https://zone01normandie.org/assets/superhero/all.json | jq --arg city "$HERO_ID" ' .[] | select(.id == $HERO_ID)  | .connections | .relatives  ' -r
+export HERO_ID
+curl https://zone01normandie.org/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" ' .[] | select(.id == $HERO_ID)  | .connections | .relatives  ' -r
