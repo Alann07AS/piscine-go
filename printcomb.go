71e7e6ef907660a99115b8da70b51a1d6b9c1d9c
Alann <SchnebelenAlann@hotmail.com>
2022-06-29 17:43:29 +0200

    salut

--- printcomb.go
+++ printcomb.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	var r rune = 48
-	for i := 48 ; i != 58 ; i++ {
+	for i := 48; i != 58 ; i++ {
 		r++
 		z01.PrintRune(r)
 		r++
@@ -16,4 +16,3 @@ func PrintComb() {
 		r = 48
 	}
 }
-
