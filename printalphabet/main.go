b7125690a7a1f8c73d978545a6081d41f4646e24
Alann <SchnebelenAlann@hotmail.com>
2022-06-29 13:55:37 +0200

    Ho My GO

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,10 +4,10 @@ import  "github.com/01-edu/z01"
 
 func main() {
 	var r rune = 96
-	for i := 96; i != 122; i++ {
+		for i := 96; i != 122; i++ {
 		r++
 		z01.PrintRune(r)
 		
 	   }
-	 z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
