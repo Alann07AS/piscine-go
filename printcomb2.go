9b9219668c205a8d71e9b82a1828279a745306b4
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 01:25:19 +0200

    s

--- printcomb2.go
+++ printcomb2.go
@@ -3,9 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	z01.PrintRune('0')
-	z01.PrintRune('0')
-	z01.PrintRune(' ')
-	z01.PrintRune('0')
-	z01.PrintRune('0')
+	const z = 48
+	var d1, d2, d3, d4 rune
+	for c := 0; c != 9900; c++ {
+		d1 = rune(c / 1000 + z)
+		d2 = rune((c / 100) % 10 + z)
+		d3 = rune((c / 10) % 10 + z)
+		d4 = rune(c % 10 + z)
+		z01.PrintRune(d1)
+		z01.PrintRune(d2)
+		z01.PrintRune(' ')
+		z01.PrintRune(d3)
+		z01.PrintRune(d4)
+		if !(int(d1) - z == 9 && int(d2) - z == 8 && int(d3) - z == 9 && int(d4) - z == 9) {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
 }
+//z01.PrintRune(rune(c1d1+48))
