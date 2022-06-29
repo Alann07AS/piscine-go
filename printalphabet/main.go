82467baa44b09e1a005912b6ef7f3d43f7a3a097
Alann <SchnebelenAlann@hotmail.com>
2022-06-29 13:41:52 +0200

    Ho My GO

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/01-edu/z01"
+import  (
+	"github.com/01-edu/z01"
+	
+)
 
 func main() {
-	
+	var r rune = 96
+	for i := 96; i != 122; i++ {
+		r++
+		z01.PrintRune(r)
+		
+	   }
+	 z01.PrintRune('\n')
+
 }
