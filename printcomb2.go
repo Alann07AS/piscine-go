6d896ab5001f4a3385db4c661994c82c12f032f7
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 10:08:44 +0200

    aze

--- printcomb2.go
+++ printcomb2.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	const z = 48
 	var d1, d2, d3, d4 rune
-	for c := 0; c != 9900; c++ {
+	for c := 1; c != 9900; c++ {
 		d1 = rune(c/1000 + z)
 		d2 = rune((c/100)%10 + z)
 		d3 = rune((c/10)%10 + z)
