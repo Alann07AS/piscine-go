d746c3ebdd597d184a2c0625c846c8bf31eea46e
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 17:19:05 +0200

    reverse

--- strrev.go
+++ strrev.go
@@ -3,9 +3,9 @@ package piscine
 func StrRev(s string) string {
 	ctemp := []byte(s)
 	b := []byte(s)
-	lenth := len(b)-1
+	lenth := len(b) - 1
 	i2 := lenth
-	for i := 0; i < lenth + 1; i++ {
+	for i := 0; i < lenth+1; i++ {
 		b[i] = ctemp[i2]
 		i2--
 	}
