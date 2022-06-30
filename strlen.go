bc46c8dcb4a08ff1c69f3109a1c430635552511d
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 16:43:48 +0200

    lensrt

--- strlen.go
+++ strlen.go
@@ -1,12 +1,13 @@
 package piscine
 
 func StrLen(s string) int {
+	c := 0
 	b := []byte(s)
 	for i := 0; len(b) > i; i++ {
-		if b[i] > 126 {
-			b[i] = 'O'
+		if b[i] > 170 {
+			c++
 		}
 	}
 
-	return len(b)
+	return len(b)-c
 }
