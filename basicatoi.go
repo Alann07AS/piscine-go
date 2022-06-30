44be534dda940cc94ee39577f020e8d0eccde4fd
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 18:16:56 +0200

    sttoint

--- basicatoi.go
+++ basicatoi.go
@@ -5,7 +5,6 @@ func BasicAtoi(s string) int {
 	lenth := len(b) - 1
 	wlenth := lenth - 1
 	var ans, weight int = 0, 1
-	
 	for i := 0; i <= lenth; i++ {
 		for i2 := wlenth; i2 >= 0; i2-- {
 			weight *= 10
