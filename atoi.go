e9a449f0ba1c50f32f9c30ad8721dc0b958b0f50
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 23:34:44 +0200

    atoi

---
+++ atoi.go
@@ -0,0 +1,36 @@
+package piscine
+
+func Atoi(s string) int {
+	b := []byte(s)
+	lenth := len(b) - 1
+	wlenth := lenth - 1
+	var ans, weight int = 0, 1
+	valid := true
+	loop := 0
+	for i := 0; i <= lenth; i++ {
+		if !(b[i] >= '0' && b[i] <= '9') {
+			valid = false
+			if i == 0 && (b[0] == '+' || b[0] == '-'){
+				valid = true
+				loop = 1
+			}
+		}
+	}
+	if valid {
+		for loop <= lenth {
+			for i2 := wlenth; i2 >= 0; i2-- {
+				weight *= 10
+			}
+			ans += (int(b[loop]) - 48) * weight
+			wlenth--
+			weight = 1
+			loop++
+		}
+	} else {
+		ans = 0
+	}
+	if b[0] == '-' {
+		ans *= -1
+	}
+	return ans
+}
