9cfcdf3dd016b6e53534555d0872065fd821ec3a
Alann <SchnebelenAlann@hotmail.com>
2022-06-30 12:13:35 +0200

    nb

--- printnbr.go
+++ printnbr.go
@@ -13,11 +13,11 @@ func Decomposeur(n int) {
 		lenght++
 		nb /= 10
 	}
-	for r :=0; r != lenght; r++ {
+	for r := 0; r != lenght; r++ {
 		dec *= 10
 	}
 	for i := 0; i <= lenght; i++ {
-		z01.PrintRune(rune((n/dec)%10+48))
+		z01.PrintRune(rune((n/dec)%10 + 48))
 		dec /= 10
 	}
 }
