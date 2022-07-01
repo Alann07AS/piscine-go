5630eaab77648aaf0ed414501aa3faecbbbaefca
Alann <SchnebelenAlann@hotmail.com>
2022-07-01 09:47:37 +0200

    testt

---
+++ sortintegertable.go
@@ -0,0 +1,18 @@
+package piscine
+
+func SortIntegerTable(table []int) {
+	// newtable := table
+	// lenth := len(table) - 1
+	// cache := 0
+	// for i := 0; i <= lenth; i++ {
+	// 	newtable[i] = table[i]
+	// }
+	// for i2 := lenth; i2 >= 0 ; i2-- {
+	// 	for i := 0; i <= lenth; i++ {
+	// 		ca
+	// 	}
+	// 	newtable[i2] = cache
+	// }
+	// return newtable
+	return 
+}
