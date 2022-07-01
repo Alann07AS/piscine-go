edbdfaee22e4b840e234703a9e6b0ac3f077f429
Alann <SchnebelenAlann@hotmail.com>
2022-07-01 11:31:02 +0200

    sort

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,18 +1,17 @@
 package piscine
 
 func SortIntegerTable(table []int) {
-	// newtable := table
-	// lenth := len(table) - 1
-	// cache := 0
-	// for i := 0; i <= lenth; i++ {
-	// 	newtable[i] = table[i]
-	// }
-	// for i2 := lenth; i2 >= 0 ; i2-- {
-	// 	for i := 0; i <= lenth; i++ {
-	// 		ca
-	// 	}
-	// 	newtable[i2] = cache
-	// }
-	// return newtable
-	return 
+	lenth := len(table) - 1
+	isModif := 1
+	for isModif != 0 {
+		isModif = 0
+		for i := 0; i < lenth; i++ {
+			if table[i] > table[i+1] {
+				t := table[i]
+				table[i] = table[i+1]
+				table[i+1] = t
+				isModif = 1
+			}
+		}
+	}
 }
