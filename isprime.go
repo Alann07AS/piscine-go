fee615bd418f3fed8ed1afe623b3447f9b42de9a
Alann <SchnebelenAlann@hotmail.com>
2022-07-04 14:20:10 +0200

    is

--- isprime.go
+++ isprime.go
@@ -2,14 +2,14 @@ package piscine
 
 func IsPrime(nb int) bool {
 	switch nb {
-		case 2:
-			return true
-		case 3:
-			return true
-		case 5:
-			return true
-		case 7:
-			return true
+	case 2:
+		return true
+	case 3:
+		return true
+	case 5:
+		return true
+	case 7:
+		return true
 	}
 	state := true
 	for i := 2; i < nb; i++ {
@@ -17,6 +17,6 @@ func IsPrime(nb int) bool {
 		if modulo == 0 && i != nb {
 			state = false
 		}
-	} 
+	}
 	return state
 }
