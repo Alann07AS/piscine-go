c2bc2c101b8c3a318fa6d0a3a176e6b148bc2016
Alann <SchnebelenAlann@hotmail.com>
2022-07-04 22:15:01 +0200

    height

--- eightqueens.go
+++ eightqueens.go
@@ -3,7 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func EightQueens() {
-	for i:= '0'; i < '9'; i++ {
-		z01.PrintRune(i)
+	for y:= 1; y <=8; y++ {
+		z01.PrintRune(rune('a'+y-1))
+
 	}
+}
+
+func recursiveIsCheck(y, nPion int) int, bool{
+
 }
