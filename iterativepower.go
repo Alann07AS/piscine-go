7f2f711a8a934e162c1c3c735bafc493c468a3f6
Alann <SchnebelenAlann@hotmail.com>
2022-07-04 11:37:57 +0200

    recu

--- iterativepower.go
+++ iterativepower.go
@@ -2,6 +2,11 @@ package piscine
 
 func IterativePower(nb, power int) int {
 	result := nb
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	}
 	for power >= 2 {
 		result *= nb
 		power--
