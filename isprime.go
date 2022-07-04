7ab851e43bed35357ee1f12d342e5f9fe4f6c120
Alann <SchnebelenAlann@hotmail.com>
2022-07-04 14:15:58 +0200

    is

---
+++ isprime.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	switch nb {
+		case 2: return true
+		case 3: return true
+		case 5: return true
+		case 7: return true
+	}
+	state := true
+	for i:=2; i < nb; i++ {
+		modulo := nb%i
+		if modulo == 0 && i != nb {
+			state = false
+		}
+	} 
+	return state
+}
