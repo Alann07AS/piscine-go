2ae7ee5678a2ee43b80faf5959385a8dfeea1e94
Alann <SchnebelenAlann@hotmail.com>
2022-07-04 11:44:49 +0200

    recu

--- recursivepower.go
+++ recursivepower.go
@@ -2,7 +2,7 @@ package piscine
 
 func RecursivePower(nb int, power int) int {
 	if power >= 1 {
-		return nb * RecursivePower(nb,power-1)
+		return nb * RecursivePower(nb, power-1)
 	}
 	if power == 0 {
 		return 1
