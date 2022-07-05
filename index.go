8aefd07eca3f8fe99822a28cdb0046fff86faea5
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 14:42:07 +0200

    Ind

--- index.go
+++ index.go
@@ -5,23 +5,22 @@ func Index(s string, toFind string) int {
 	tabbleToFind := []rune(toFind)
 	lS := len(tabbleS) - 1
 	lF := len(tabbleToFind) - 1
-	isFind := true
-	result := 0
 	for i := 0; i <= lS; i++ {
 		if tabbleS[i] == tabbleToFind[0] {
-			for i2 := 0; i2 <= lF; i2++ {
-				if tabbleS[i+i2] != tabbleToFind[i2] {
-					i2 = lF + 1
-					isFind = false
-				}
-			}
-			if isFind {
-				result++
+			if isTrue(lF, i, tabbleS, tabbleToFind) {
+				return i
 			}
 		}
 	}
-	if result == 0 {
-		result = -1
+	return -1
+}
+
+func isTrue(lF, i int, tabbleS, tabbleToFind []rune) bool {
+	for i2 := 0; i2 <= lF; i2++ {
+		if tabbleS[i+i2] != tabbleToFind[i2] {
+			i2 = lF + 1
+			return false
+		}
 	}
-	return result + 1
+	return true
 }
