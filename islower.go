f526cd87977f53ad2b4a7d140502453a940f6753
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 15:16:40 +0200

    islo

---
+++ islower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsLower(s string) bool {
+	tabble := []rune(s)
+	l := len(s) - 1
+	for i := 0; i <= l; i++ {
+		if !(tabble[i] > 'a' && tabble[i] < 'z') {
+			return false
+		}
+	}
+	return true
+}
