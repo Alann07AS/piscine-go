336b68ba5f31678daa3f38f8e59b812ed110258e
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 16:03:55 +0200

    tout

--- isupper.go
+++ isupper.go
@@ -4,7 +4,7 @@ func IsUpper(s string) bool {
 	tabble := []rune(s)
 	l := len(s) - 1
 	for i := 0; i <= l; i++ {
-		if !(tabble[i] > 'A' && tabble[i] < 'Z') {
+		if !(tabble[i] >= 'A' && tabble[i] <= 'Z') {
 			return false
 		}
 	}
