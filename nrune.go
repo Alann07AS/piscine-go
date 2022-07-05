5307c274ee8b3b194a87b9f5224f7a5e385fb15e
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 10:17:05 +0200

    nr

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	tabble := []rune(s)
 	l := len(tabble)
-	if n <= l && n >= 0 {
+	if n <= l && n > 0 {
 		return tabble[n-1]
 	} else {
 		return 0
