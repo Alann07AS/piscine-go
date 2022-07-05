96221bace627037447e77732f7df029ee21db451
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 16:00:54 +0200

    toup

--- toupper.go
+++ toupper.go
@@ -5,7 +5,7 @@ func ToUpper(s string) string {
 	l := len(s) - 1
 	for i := 0; i <= l; i++ {
 		if IsLower(string(tabble[i])) {
-			tabble[i] -= 0x20
+			tabble[i] += 0x20
 		}
 	}
 	return string(tabble)
