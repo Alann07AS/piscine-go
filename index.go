a586754d9fc5ad03b2e7be0425a6e44965d55ccf
Alann <SchnebelenAlann@hotmail.com>
2022-07-05 11:48:10 +0200

    index

---
+++ index.go
@@ -0,0 +1,24 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	tabbleS := []rune(s)
+	tabbleToFind := []rune(toFind)
+	lS := len(tabbleS) - 1
+	lF := len(tabbleToFind) - 1
+	isFind := true
+	result := 0
+	for i := 0; i <= lS; i++ {
+		if tabbleS[i] == tabbleToFind[0] {
+			for i2 := 0; i2 <= lF; i2++ {
+				if tabbleS[i+i2] != tabbleToFind[i2] {
+					i2 = lF + 1
+					isFind = false
+				}
+			}
+			if isFind {
+				result++
+			}
+		}
+	}
+	return result
+}
