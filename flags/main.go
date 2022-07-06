bd880eb28747293e7edc021ee1aaa8cb3e05dc30
Alann <SchnebelenAlann@hotmail.com>
2022-07-06 23:40:09 +0200

    terminal

--- flags/main.go
+++ flags/main.go
@@ -87,10 +87,10 @@ func concatArgs(str1 string, str2 string) string {
 func printHelp() {
 	help := []string{
 		"--insert\n",
-		" -i\n",
+		"  -i\n",
 		"	This flag inserts the string into the string passed as argument.\n",
 		"--order\n",
-		" -o\n",
+		"  -o\n",
 		"	This flag will behave like a boolean, if it is called it will order the argument.\n",
 	}
 	for _, txt := range help {
