029a83ecbd44e396dfc0222b007e4c6d73abc59d
Alann <SchnebelenAlann@hotmail.com>
2022-07-06 13:49:24 +0200

    revvv

---
+++ revparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	s := os.Args
+	for i := len(s) - 1; i >= 1; i++ {
+		sTable := []rune(s[i])
+		for _, name := range sTable {
+			z01.PrintRune(name)
+		}
+		z01.PrintRune('\n')
+	}
+}
