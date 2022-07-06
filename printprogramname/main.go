a310a1c0a8e1c514289f14d8ef394e124d5c34b0
Alann <SchnebelenAlann@hotmail.com>
2022-07-06 09:28:42 +0200

    printpro

---
+++ printprogramname/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	s := os.Args
+	sTable := []rune(s[0])
+	for _, name := range sTable {
+		z01.PrintRune(name)
+	}
+	z01.PrintRune('\n')
+}
