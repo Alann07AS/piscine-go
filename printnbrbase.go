1367daa817c67fee5abc8df1c11b81972b392b62
Alann <SchnebelenAlann@hotmail.com>
2022-07-07 23:04:02 +0200

    printBase

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,34 +1,68 @@
 package piscine
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
 
 func PrintNbrBase(nbr int, base string) {
-	result := 0
-	weight := 0
-	tabble := [40]rune{}
-	finaltable := []rune{}
-	lastTabble := []rune{}
-	baseInt := len(base)
-	for i := 0; i <= baseInt-1 && result != nbr; i++ {
-		tabble[0]++
-		result++
-		lastTabble = recursiveCalc(baseInt, weight, tabble[:])
-		if result == nbr {
-			finaltable = lastTabble
-		}
-		fmt.Println(result) // nombre de tour dans ma boucle
+	baseTable := []rune(base)
+	var intTbale []int
+	baseNb := len(baseTable) - 1
+	index := 0
+	isNeg := false
+	if !sortAndCheckBase(baseTable) {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
 	}
-	fmt.Println((finaltable))
+	if nbr < 0 {
+		nbr *= -1
+		isNeg = true
+	}
+	for calc := 0; calc < nbr; calc++ {
+		intTbale = recursifBaseCalc(baseNb, index, intTbale)
+		// fmt.Println(intTbale) //Suprimer FMT !!!!!______________
+	}
+	if isNeg {
+		z01.PrintRune('-')
+	}
+	for i := len(intTbale) - 1; i >= 0; i-- {
+		z01.PrintRune(baseTable[intTbale[i]])
+	}
 }
 
-func recursiveCalc(base, weight int, tabble []rune) []rune {
-	if tabble[weight] == rune(base) {
-		recursiveCalc(base, weight+1, tabble)
-	} else {
-		tabble[weight]++
-		return tabble
+func recursifBaseCalc(baseNb, index int, inTable []int) []int {
+	l := len(inTable) - 1
+	if index > l {
+		inTable = append(inTable, 0)
 	}
-	return tabble
+	if inTable[index] != baseNb {
+		inTable[index]++
+		return inTable
+	} else {
+		inTable[index] = 0
+		return recursifBaseCalc(baseNb, index+1, inTable)
+	}
+}
+
+func sortAndCheckBase(s []rune) bool {
+	isModifie := true
+	for isModifie {
+		isModifie = false
+		for i := len(s) - 1; i >= 1; i-- {
+			var a rune
+			if i >= 1 && s[i-1] > s[i] {
+				a = s[i]
+				s[i] = s[i-1]
+				s[i-1] = a
+				isModifie = true
+			}
+		}
+	}
+	for i := len(s) - 1; i >= 1; i-- {
+		if i >= 1 && s[i-1] == s[i] {
+			return false
+		}
+	}
+	return true
 }
