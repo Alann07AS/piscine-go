e0546a22848168fa4e5afa26448c8f7b002d845c
Alann <SchnebelenAlann@hotmail.com>
2022-07-07 13:28:16 +0200

    qsd

---
+++ splitwhitespaces.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "fmt"
+
+func SplitWhiteSpaces(s string) []string {
+	table := []rune(s)
+	var sTable []string
+	indexD := 0
+	l := 17
+	for i := 0; i <= l; i++ {
+		fmt.Print(i)
+		if table[i] == ' ' || table[i] == '	' || table[i] == '\n' || i == l {
+			if i == l {
+				sTable = append(sTable, string(table[indexD:l+1]))
+			} else {
+				sTable = append(sTable, string(table[indexD:i]))
+				indexD = i + 1
+			}
+		}
+	}
+	return sTable
+}
