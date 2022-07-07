5163024bafc7d66e0c2884e49f45834d8332a9fb
Alann <SchnebelenAlann@hotmail.com>
2022-07-07 13:28:42 +0200

    qsd

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,14 +1,11 @@
 package piscine
 
-import "fmt"
-
 func SplitWhiteSpaces(s string) []string {
 	table := []rune(s)
 	var sTable []string
 	indexD := 0
 	l := 17
 	for i := 0; i <= l; i++ {
-		fmt.Print(i)
 		if table[i] == ' ' || table[i] == '	' || table[i] == '\n' || i == l {
 			if i == l {
 				sTable = append(sTable, string(table[indexD:l+1]))
