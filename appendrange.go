3a190fe24823937a25a242750438e4971fd7f53a
Alann <SchnebelenAlann@hotmail.com>
2022-07-07 10:55:08 +0200

    appen

---
+++ appendrange.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	size := max - min
+	if size < 0 {
+		table := []int{}
+		return table
+	}
+	table := make([]int, size)
+	for i := min; i < max; i++ {
+		table[i-min] = i
+	}
+	return table
+
+}
