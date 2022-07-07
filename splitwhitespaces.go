9284516343766a8a06b10ae1c9fadf137931d447
Alann <SchnebelenAlann@hotmail.com>
2022-07-07 13:46:37 +0200

    qsdq

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,7 +4,7 @@ func SplitWhiteSpaces(s string) []string {
 	table := []rune(s)
 	var sTable []string
 	indexD := 0
-	l := 17
+	l := len(table) - 1
 	for i := 0; i <= l; i++ {
 		if table[i] == ' ' || table[i] == '	' || table[i] == '\n' || i == l {
 			if i == l {
