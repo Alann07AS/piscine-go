91b491732fbe2b50831163f7a8c23809eaf40030
Alann <SchnebelenAlann@hotmail.com>
2022-07-08 11:35:24 +0200

    bbb

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,29 +1,27 @@
 package piscine
 
 import (
-	"fmt"
-
 	"github.com/01-edu/z01"
 )
 
 func PrintNbrBase(nbr int, base string) {
-	baseTable := []rune(base)
+	baseTableBefore := []rune(base)
 	var intTbale []int
-	baseNb := len(baseTable) - 1
+	baseNb := len(baseTableBefore) - 1
 	index := 0
 	isNeg := false
-	if !sortAndCheckBase(baseTable) || baseNb <= 0 {
+	if !sortAndCheckBase(baseTableBefore) || baseNb <= 0 {
 		z01.PrintRune('N')
 		z01.PrintRune('V')
 		return
 	}
+	baseTable := []rune(base)
 	if nbr < 0 {
 		nbr *= -1
 		isNeg = true
 	}
 	for calc := 0; calc < nbr; calc++ {
 		intTbale = recursifBaseCalc(baseNb, index, intTbale)
-		fmt.Println(intTbale) //Suprimer FMT !!!!!______________
 	}
 	if isNeg {
 		z01.PrintRune('-')
@@ -47,7 +45,9 @@ func recursifBaseCalc(baseNb, index int, inTable []int) []int {
 	}
 }
 
-func sortAndCheckBase(s []rune) bool {
+func sortAndCheckBase(sBase []rune) bool {
+	var s []rune
+	s = sBase
 	isModifie := true
 	for isModifie {
 		isModifie = false
