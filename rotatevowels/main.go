3bf7a8590107769b65a942ae6c0196a5ff5af233
Alann <SchnebelenAlann@hotmail.com>
2022-07-08 08:57:50 +0200

    rotatefow

---
+++ rotatevowels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	sTable := os.Args
+	sTtoWrite := ""
+	if len(sTable) >= 1 {
+		sTtoWrite += sTable[1]
+		for i := 2; i <= len(sTable)-1; i++ {
+			sTtoWrite += " "
+			sTtoWrite += sTable[i]
+		}
+	}
+	runeTable := []rune(sTtoWrite)
+	var fowelTable []rune
+	for _, char := range runeTable {
+		if isFowel(char) {
+			fowelTable = append(fowelTable, char)
+		}
+	}
+	i := len(fowelTable) - 1
+	for _, char := range runeTable {
+		if isFowel(char) {
+			z01.PrintRune(fowelTable[i])
+			i--
+		} else {
+			z01.PrintRune(char)
+		}
+	}
+
+}
+
+func isFowel(r rune) bool {
+	switch r {
+	case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	}
+	return false
+}
