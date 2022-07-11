5a8d506403e6d953b37235d3b3503c04a1dbb9ad
Alann <SchnebelenAlann@hotmail.com>
2022-07-11 13:29:25 +0200

    aaaa

--- cat/main.go
+++ cat/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
@@ -10,13 +11,20 @@ func main() {
 	for i := 1; i <= len(args)-1; i++ {
 		file, err := os.Open(args[i])
 		if err != nil {
-			fmt.Printf("ERROR: %v\n", err.Error())
+			printStr("ERROR: ")
+			printStr(err.Error())
 			return
 		}
 		stat, _ := file.Stat()
 		contennet := make([]byte, stat.Size())
 		file.Read(contennet)
-		fmt.Print(string(contennet))
+		printStr(string(contennet))
 		file.Close()
+	}
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
 	}
 }
