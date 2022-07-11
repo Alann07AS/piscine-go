6803106a482ea9f2954608829cb2947195d86632
Alann <SchnebelenAlann@hotmail.com>
2022-07-11 11:12:47 +0200

    dispal

--- displayfile/main.go
+++ displayfile/main.go
@@ -14,13 +14,10 @@ func main() {
 		fmt.Println("Too much arguents appear")
 		return
 	}
-
 	file, err := os.Open("quest8.txt")
-
 	if err != nil {
 		fmt.Printf("the mistake is : %v\n", err.Error())
 	}
-
 	stat, _ := file.Stat()
 	contennet := make([]byte, stat.Size())
 	file.Read(contennet)
