69b0a71a40c5f36a58d1d4993ac86f245a293b66
Alann <SchnebelenAlann@hotmail.com>
2022-07-11 14:33:46 +0200

    zdqsdqs

--- cat/main.go
+++ cat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
@@ -8,11 +9,18 @@ import (
 
 func main() {
 	args := os.Args
+	bytes, err1 := ioutil.ReadAll(os.Stdin)
+	if err1 == nil {
+		printStr(string(bytes))
+		return
+	}
 	for i := 1; i <= len(args)-1; i++ {
 		file, err := os.Open(args[i])
 		if err != nil {
 			printStr("ERROR: ")
 			printStr(err.Error())
+			os.Exit(1)
+			z01.PrintRune('\n')
 			return
 		}
 		stat, _ := file.Stat()
