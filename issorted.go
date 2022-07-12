ea6e9d8383157b416c88daf0cad716fd2b9b81d6
Alann <SchnebelenAlann@hotmail.com>
2022-07-12 11:07:44 +0200

    issorted

---
+++ issorted.go
@@ -0,0 +1,29 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	startNeg := false
+	for i := range a {
+		if f(a[i], a[i+1]) < 0 {
+			startNeg = true
+			break
+		}
+		if f(a[i], a[i+1]) > 0 {
+			startNeg = false
+			break
+		}
+	}
+	if startNeg {
+		for i := 0; i < len(a)-1; i++ {
+			if f(a[i], a[i+1]) > 0 {
+				return false
+			}
+		}
+	} else {
+		for i := 0; i < len(a)-1; i++ {
+			if f(a[i], a[i+1]) < 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
