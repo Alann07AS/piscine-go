ce1ae3d550b682c2df5c852fc99ceced7c0c2b4f
Alann <SchnebelenAlann@hotmail.com>
2022-07-12 13:19:46 +0200

    sdsq

--- doop/main.go
+++ doop/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"piscine"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
@@ -17,25 +18,92 @@ func main() {
 	result := 0
 	switch args[2] {
 	case "*":
-		result = piscine.Atoi(args[1]) * piscine.Atoi(args[3])
+		result = Atoi(args[1]) * piscine.Atoi(args[3])
 	case "+":
-		result = piscine.Atoi(args[1]) + piscine.Atoi(args[3])
+		result = Atoi(args[1]) + piscine.Atoi(args[3])
 	case "-":
-		result = piscine.Atoi(args[1]) - piscine.Atoi(args[3])
+		result = Atoi(args[1]) - piscine.Atoi(args[3])
 	case "/":
 		if args[3] != "0" {
-			result = piscine.Atoi(args[1]) / piscine.Atoi(args[3])
+			result = Atoi(args[1]) / piscine.Atoi(args[3])
 		} else {
-			fmt.Println("No division by 0")
+			printStr("No division by 0")
 			return
 		}
 	case "%":
 		if args[3] != "0" {
-			result = piscine.Atoi(args[1]) % piscine.Atoi(args[3])
+			result = Atoi(args[1]) % piscine.Atoi(args[3])
 		} else {
-			fmt.Println("No modulo by 0")
+			printStr("No modulo by 0")
 			return
 		}
 	}
-	fmt.Println(result)
+
+	printStr(intToString(result))
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func Atoi(s string) int {
+	var ans, weight int = 0, 1
+	if !(s == "") {
+		b := []byte(s)
+		lenth := len(b) - 1
+		wlenth := lenth - 1
+		valid := true
+		loop := 0
+		for i := 0; i <= lenth; i++ {
+			if !(b[i] >= '0' && b[i] <= '9') {
+				valid = false
+				if i == 0 && (b[0] == '+' || b[0] == '-') {
+					valid = true
+					loop = 1
+					wlenth -= 1
+				}
+			}
+		}
+		if valid {
+			for loop <= lenth {
+				for i2 := wlenth; i2 >= 0; i2-- {
+					weight *= 10
+				}
+				ans += (int(b[loop]) - 48) * weight
+				wlenth--
+				weight = 1
+				loop++
+			}
+		} else {
+			ans = 0
+		}
+		if b[0] == '-' {
+			ans *= -1
+		}
+	} else {
+		ans = 0
+	}
+	return ans
+}
+
+func intToString(nb int) string {
+	str := ""
+	l := 0
+	nbTemp := nb
+	weight := 1
+	for nbTemp != 0 {
+		l++
+		nbTemp /= 10
+	}
+	for i := 1; i < l; i++ {
+		weight *= 10
+	}
+	for i := 1; i <= l; i++ {
+		str += string(rune(((nb / weight) % 10) + 48))
+		weight /= 10
+	}
+	return str
 }
