3d0d2c68a55523eca713755c1f47026212a1859e
Alann <SchnebelenAlann@hotmail.com>
2022-07-14 22:35:57 +0200

    fix

---
+++ fixthemain/main.go
@@ -0,0 +1,50 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state bool
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func OpenDoor(Door *Door) {
+	PrintStr("Door Opening...")
+	Door.state = OPEN
+}
+
+func CloseDoor(Door *Door) {
+	PrintStr("Door Closing...")
+	Door.state = CLOSE
+}
+
+func IsDoorOpen(Door *Door) bool {
+	PrintStr("is the Door opened ?")
+	return Door.state
+}
+
+func IsDoorClose(Door *Door) bool {
+	PrintStr("is the Door closed ?")
+	return Door.state
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
+
+const OPEN, CLOSE = true, false
