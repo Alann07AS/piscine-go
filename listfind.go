2cbf2d3c74ec9e6ac8b3e1571da6482cffe9a0f1
Alann <SchnebelenAlann@hotmail.com>
2022-07-18 20:36:19 +0200

    find

---
+++ listfind.go
@@ -0,0 +1,9 @@
+package piscine
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	return &l.Head.Data
+}
