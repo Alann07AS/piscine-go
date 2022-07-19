67cfa518751359677ae1464f7bc13645b678d9a8
Alann <SchnebelenAlann@hotmail.com>
2022-07-19 18:45:03 +0200

    countlevel

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -1,20 +1,14 @@
 package piscine
 
-import "fmt"
-
 func BTreeLevelCount(root *TreeNode) int {
 	return levelCnt(root, 0)
 }
 
 func levelCnt(root *TreeNode, comp int) int {
 	if root.Left != nil {
-		fmt.Println(comp)
-
 		comp = levelCnt(root.Left, comp+1)
 	}
 	if root.Right != nil {
-		fmt.Println(comp)
-
 		comp = levelCnt(root.Right, comp+1)
 	}
 	return comp
