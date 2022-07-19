6aec91ad51d8b14d82bf197fec5e6ad6fcafa3fd
Alann <SchnebelenAlann@hotmail.com>
2022-07-19 15:11:36 +0200

    root

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,12 +1,25 @@
 package piscine
 
+import "fmt"
+
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if elem == root.Data {
+		fmt.Println(root.Data)
 		return root
 	}
-	if elem <= root.Data {
-		return BTreeSearchItem(root.Left, elem)
+	if elem < root.Data {
+		if root.Left != nil {
+			root.Left.Parent = root
+			return BTreeSearchItem(root.Left, elem)
+		} else {
+			return nil
+		}
 	} else {
-		return BTreeSearchItem(root.Right, elem)
+		if root.Left != nil {
+			root.Right.Parent = root
+			return BTreeSearchItem(root.Right, elem)
+		} else {
+			return nil
+		}
 	}
 }
