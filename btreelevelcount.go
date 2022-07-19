f6cb03f77f5634cb6dab1e75c126a8d4152b4a8e
Alann <SchnebelenAlann@hotmail.com>
2022-07-19 15:52:50 +0200

    level comp

---
+++ btreelevelcount.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BTreeLevelCount(root *TreeNode) int {
+	return levelCnt(root, 0)
+}
+
+func levelCnt(root *TreeNode, comp int) int {
+	if root.Left != nil {
+		comp = levelCnt(root.Left, comp+1)
+	}
+	if root.Right != nil {
+		comp = levelCnt(root.Right, comp+1)
+	}
+	return comp
+}
