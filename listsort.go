ad53f3ec4c641c428a562bc4262b08680b1aa9bb
Alann <SchnebelenAlann@hotmail.com>
2022-07-20 18:17:50 +0200

    listsort

---
+++ listsort.go
@@ -0,0 +1,22 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	isMod := true
+	for isMod {
+		isMod = false
+		it := l
+		for it.Next != nil {
+			if it.Data > it.Next.Data {
+				isMod = true
+				it.Data, it.Next.Data = it.Next.Data, it.Data
+			}
+			it = it.Next
+		}
+	}
+	return l
+}
