825f5fb119eea9bf4613b917b3cb44a055afa7c8
Alann <SchnebelenAlann@hotmail.com>
2022-07-20 16:47:37 +0200

    merge

--- listmerge.go
+++ listmerge.go
@@ -1,8 +1,12 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	if l2 == nil || l1 == nil {
-		return
+	it := l1.Head
+	for it != nil {
+		if it.Next == nil {
+			l1.Tail = it
+		}
+		it = it.Next
 	}
 	l1.Tail.Next = l2.Head
 	l1.Tail = l2.Tail
