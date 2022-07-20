f0a590eb1459d2e729489a9867e7af152eaa059e
Alann <SchnebelenAlann@hotmail.com>
2022-07-20 17:13:57 +0200

    sqdqs

--- listmerge.go
+++ listmerge.go
@@ -1,16 +1,16 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	if l2.Head.Next == nil {
+	if l1.Head == nil || l2.Head == nil {
 		return
 	}
-	it := l1.Head
-	for it != nil {
-		if it.Next == nil {
-			l1.Tail = it
-		}
-		it = it.Next
-	}
+	// it := l1.Head
+	// for it != nil {
+	// 	if it.Next == nil {
+	// 		l1.Tail = it
+	// 	}
+	// 	it = it.Next
+	// }
 	l1.Tail.Next = l2.Head
 	l1.Tail = l2.Tail
 }
