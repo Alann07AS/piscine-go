3ee1bd4445f18a7f18ec5e59b27da779f3a4c33c
Alann <SchnebelenAlann@hotmail.com>
2022-07-21 10:34:14 +0200

    isbinar

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -1,16 +1,20 @@
 package piscine
 
 func BTreeIsBinary(root *TreeNode) bool {
-	var isBinary bool = true
+	var isBinary bool = false
 	if root.Left != nil && root.Left.Data > root.Data {
 		return false
 	} else if root.Left != nil {
 		isBinary = BTreeIsBinary(root.Left)
+	} else {
+		return true
 	}
 	if root.Right != nil && root.Right.Data <= root.Data {
 		return false
 	} else if root.Right != nil {
 		isBinary = BTreeIsBinary(root.Right)
+	} else {
+		return true
 	}
 	return isBinary
 }
